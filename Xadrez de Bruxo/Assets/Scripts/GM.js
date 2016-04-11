28c2f76077d9dd35182971cef0977bcb20ae6efa
italogsfernanes <italogsfernandes@gmail.com>
2016-04-10 21:26:21 -0300

    Unity project started!

---
+++ Xadrez de Bruxo/Assets/Scripts/GM.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+private var objSelecionado: Transform;
+private var piece: Transform;
+private var local: int;
+private var switcher: boolean;
+function Start () {
+	switcher = false;
+
+}
+
+function FixedUpdate () {
+	if (Input.GetButtonDown("Fire1")) {
+            var hit: RaycastHit;
+            var ray: Ray;
+			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+      		
+
+            if (Physics.Raycast(ray, hit, 100)) 
+            {
+     			objSelecionado = hit.transform;
+     		
+ 				if(objSelecionado.CompareTag("Board")){
+ 					local = Mathf.RoundToInt(hit.point.x) + Mathf.RoundToInt(hit.point.z)*10;
+ 					Debug.Log ("Board: " + hit.point.ToString () + 
+					 	", Local = " + 	local );
+					 if(switcher){
+						switcher = false;
+						piece.position.x = local%10;
+						piece.position.z = local/10;
+					
+					}
+     			} else {
+     				local = Mathf.RoundToInt(objSelecionado.position.x)+ Mathf.RoundToInt(objSelecionado.position.z)*10;
+					Debug.Log ("Piece: " + objSelecionado.name.ToString () + 
+						 	", Local = " + 	local );
+					 piece = objSelecionado;
+					 switcher = true;
+ 				}
+            }   
+        }
+}
